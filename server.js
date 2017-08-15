5599ac1fc680a944353ea7193365256090e92409
jitendra24498 <30659312+jitendra24498@users.noreply.github.com>
2017-08-15 22:54:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artile one', function (req, res) {
+app.get('/artile-one', function (req, res) {
     res.send('Article one will be served here');
 });
-app.get('/article two', function (req, res) {
+app.get('/article-two', function (req, res) {
     res.send('Article two will be served here');
 });
-app.get('/article three', function (req, res) {
+app.get('/article-three', function (req, res) {
     res.send('Article three will be served here');
 });
 
